f38edb44615597d7473048491b6679cde1412ab7
nhandndhe180152 <nhandndhe180152@fpt.edu.vn>
2026-06-25 18:58:05 +0700

    thêm các bảng theo dõi audit log

--- Backend.Infrastructure/Constants/CommonConstants.cs
+++ Backend.Infrastructure/Constants/CommonConstants.cs
@@ -8,6 +8,28 @@ public static class CommonConstants
     public static readonly string[] AuditedEntityNames = new[]
     {
             nameof(User),
+            nameof(Warehouse),
+            nameof(Location),
+            nameof(Product),
+            nameof(ProductCategory),
+            nameof(ProductVariant),
+            nameof(InboundOrder),
+            nameof(InboundOrderItem),
+            nameof(OutboundOrder),
+            nameof(OutboundOrderItem),
+            nameof(Inventory),
+            nameof(InventoryTransaction),
+            nameof(IotDevice),
+            nameof(IotDeviceCommand),
+            nameof(Supplier),
+            nameof(CustomerReturnOrder),
+            nameof(CustomerReturnOrderItem),
+            nameof(ReturnToSupplierOrder),
+            nameof(ReturnToSupplierOrderItem),
+            nameof(StockAlertConfig),
+            nameof(StockTake),
+            nameof(StockTakeItem),
+            nameof(UnitOfMeasure)
     };
 }
 
