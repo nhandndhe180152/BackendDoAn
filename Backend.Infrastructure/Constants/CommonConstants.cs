a4ed70d99a27249162412189d0edb6fd46645801
nhandndhe180152 <nhandndhe180152@fpt.edu.vn>
2026-07-03 19:09:35 +0700

    đẩy code đẩy noti realtime cho product attribute

--- Backend.Infrastructure/Constants/CommonConstants.cs
+++ Backend.Infrastructure/Constants/CommonConstants.cs
@@ -49,6 +49,7 @@ public static class CommonConstants
             "Product",
             "ProductCategory",
             "ProductVariant",
+            "ProductAttribute",
             "Supplier",
             "UnitOfMeasure",
             "IotDevice",
