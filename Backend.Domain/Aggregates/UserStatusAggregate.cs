0dfc8f8e60fd00ca037e80940f968dcb199a46fa
nhandndhe180152 <nhandndhe180152@fpt.edu.vn>
2026-07-14 08:55:37 +0700

    sửa lại code để hiện mã của action và userstatus

--- Backend.Domain/Aggregates/UserStatusAggregate.cs
+++ Backend.Domain/Aggregates/UserStatusAggregate.cs
@@ -5,6 +5,7 @@ namespace Backend.Domain.Aggregates;
 public class UserStatusAggregate
 {
     public int Id { get; set; }
+    public string? Code { get; set; }
     public string Name { get; set; } = null!;
     public string Description { get; set; } = "";
     public string Color { get; set; } = null!;
